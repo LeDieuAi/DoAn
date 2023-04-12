fb30fb96228b9e94345f0000203d00cda6baf6f8
LeDieuAi <128711182+LeDieuAi@users.noreply.github.com>
2023-04-12 13:59:23 +0700

    Update DoanVien.java

--- app/src/main/java/com/example/doan/DoanVien.java
+++ app/src/main/java/com/example/doan/DoanVien.java
@@ -4,11 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DoanVien extends AppCompatActivity {
-
+SQLiteDatabase database = null;
+        String SKPT ="SKPT.db";
+    EditText editMaDV,editTenDv,editNgaySinh,editphai,editdchi;
+         ListView listShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doan_vien);
 
+        
+        
+        
+        
+        
+        
+        
     }
 }
